172c61f393d10f38354c67a3b41f8b5060794b82
Kailash Rawat <you@example.com>
2023-12-13 16:22:13 +0530

    mm

---
+++ src/components/ReactAssignment1.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function ReactAssignment1() {
+  return (
+    <>
+      Ques1:What is use of Gitignore File in React Application?<br /><br />
+
+      Ans:A .gitignore file in project root folder tells Git, what all files to ignore from tracking.<br /><br />
+
+      Ques2:How we can create the React Application? Explain the all Steps for them?<br /><br />
+
+      Ans:First we install nodejs and tehn  NPM(Node Package Manager) and most specifically code editor then we create command npx create-react-app "react app name" in code editor like VS code after create  react app then we hit one more command npm start, then our react app will run on localhost port no-3000 on browser.<br /><br />
+
+      Ques3:What do you know about NPM?<br /><br />
+
+      Ans:First of all NPM means (node package manager) which is very mandatory to create react app and why it is so important because NPM is a library where all react app dependency like app name and how many dependency we have downloaded for creating react app, it will show on node package manager.<br /><br />
+
+      Ques4:What are the advantages of using React?<br /><br />
+ 
+      Ans:It is actively maintained by Meta and a community of skilled developers and companies. It is known to be fast, scalable, simple, and highly advantageous to be used to create large web applications where we can change data without reloading the page.<br /><br />
+
+      Ques5:What are the limitations of React?<br /><br />
+
+      Ans:It’s a Library, Not a Framework.
+          It Uses JSX.
+          Does Not Support SEO.
+          Lack of Updated Documentation.
+
+
+    </>
+  )
+}
+
+export default ReactAssignment1
